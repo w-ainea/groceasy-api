ee4b721578fe7483d8ea297caa645c1b3b91f76e
Ainea Wabwoba <aineah28@gmail.com>
2020-10-14 10:03:22 +0300

    set up endpoints for Lipa na MPESA

--- src/routes/checkout.js
+++ src/routes/checkout.js
@@ -1,43 +1,108 @@
 const express = require("express");
+const { auth } = require("express-openid-connect");
+
+const request = require("request");
+require("dotenv").config();
 
 const router = express.Router();
 
-// const { initiatePayment } = require("../actions").checkout;
+const authenticate = (req, res, next) => {
+  const consumer_key = process.env.API_CONSUMER_KEY;
+  const consumer_secret = process.env.API_CONSUMER_SECRET;
+  let url =
+    "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+  let auth =
+    "Basic " +
+    new Buffer.from(consumer_key + ":" + consumer_secret).toString("base64");
 
-router.post("/pay", (req, res) => {
-  const axios = require("axios"),
-    oauth_token = "qH5EbtU6R56lhAQcAh2ZtRtSrFfx",
-    url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
-    auth = "Bearer " + oauth_token;
-
-  axios(
+  request(
     {
-      method: "POST",
       url,
+      headers: {
+        Authorization: "Basic" + auth,
+      },
+    },
+    (error, response, body) => {
+      // TODO: Use the body object to extract OAuth access token
+      if (error) {
+        res.send(error);
+      } else {
+        req.access_token = JSON.parse(body).access_token;
+
+        next;
+      }
+
+      res.status(200).json(body);
+    }
+  );
+};
+
+router.get("/stk", authenticate, (req, res) => {
+  let url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
+    auth = "Bearer " + req.access_token,
+    date = new Date(),
+    timestamp =
+      date.getFullYear() +
+      "" +
+      "" +
+      date.getMonth() +
+      "" +
+      "" +
+      date.getDate() +
+      "" +
+      "" +
+      date.getHours() +
+      "" +
+      "" +
+      date.getMinutes() +
+      "" +
+      "" +
+      date.getSeconds();
+
+  const password = new Buffer.from(
+    "174379" +
+      "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919" +
+      timestamp
+  ).toString("base64");
+
+  request(
+    {
+      url: url,
+      method: "POST",
       headers: {
         Authorization: auth,
       },
       json: {
-        BusinessShortCode: " ",
-        Password: " ",
-        Timestamp: " ",
+        BusinessShortCode: "174379",
+        Password: password,
+        Timestamp: timestamp,
         TransactionType: "CustomerPayBillOnline",
-        Amount: " ",
-        PartyA: " ",
-        PartyB: " ",
-        PhoneNumber: " ",
-        CallBackURL: "https://ip_address:port/callback",
-        AccountReference: " ",
-        TransactionDesc: " ",
+        Amount: "1",
+        PartyA: "254715390163",
+        PartyB: "174379",
+        PhoneNumber: "254715390163",
+        CallBackURL: " https://192.168.0.107:8000/checkout/pay",
+        AccountReference: "Test",
+        TransactionDesc: "TestPay",
       },
     },
     function (error, response, body) {
-      // TODO: Use the body object to extract the response
-      console.log(body);
+      if (error) {
+        console.log(error);
+      } else {
+        res.status(200).json(body);
+      }
     }
   );
+});
 
-  // res.send("Payment Initiated");
+router.get("/auth", authenticate, (req, res) => {
+  res.status(200).json({ access_token: req.access_token });
+});
+
+router.post("/pay", (req, res) => {
+  console.log("-------STK-------");
+  console.log(body);
 });
 
 module.exports = router;
